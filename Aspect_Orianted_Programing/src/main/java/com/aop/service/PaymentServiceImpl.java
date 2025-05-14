2e714005895d339e7bb1a7b389db5d433f1f2870
Vinayak Bhandare <vinayakbhandare2000@gmail.com>
2025-05-14 10:56:08 +0530

    Add files via upload

---
+++ Aspect_Orianted_Programing/src/main/java/com/aop/service/PaymentServiceImpl.java
@@ -0,0 +1,20 @@
+package com.aop.service;
+
+public class PaymentServiceImpl implements PaymentService {
+
+	@Override
+	public void makePayment() {
+		//payment code
+		
+		System.out.println("Amount Credited....!");
+		//
+		//
+		//
+		//
+		System.out.println("Amount Debited...!");
+		
+	}
+	
+	
+
+}
