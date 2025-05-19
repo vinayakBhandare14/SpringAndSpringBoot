f745fc59927be5246a3d95dba4e663f94c1e6f85
Vinayak Bhandare <vinayakbhandare2000@gmail.com>
2025-05-19 19:18:51 +0530

    Add files via upload

---
+++ ServaletDemo1/src/main/java/servletclasses/WelcomeServlet.java
@@ -0,0 +1,21 @@
+package servletclasses;
+import java.io.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class WelcomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Welcome to the Servlet World!</h1>");
+        out.println("</body></html>");
+    }
+}
---
+++ ServaletDemo1/src/main/webapp/WEB-INF/classes/servletclasses/WelcomeServlet.java
@@ -0,0 +1,18 @@
+package servletclasses;
+import java.io.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Welcome to the Servlet World!</h1>");
+        out.println("</body></html>");
+    }
+}
